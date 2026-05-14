ebd41177e7b5c8d6c2a6561ea8398f01317c2356
mongpt <mongphantan@gmail.com>
2026-05-14 21:08:08 +0300

    Cleanup: remove all nested Debug folders from tracking

--- examples/stm32l432/Debug/st7789/fonts.d
+++
@@ -1,2 +0,0 @@
-st7789/fonts.o: ../st7789/fonts.c ../st7789/fonts.h
-../st7789/fonts.h:
--- examples/test_ST7789_LCD/Debug/Drivers/ST7789/fonts.d
+++
@@ -1,3 +0,0 @@
-Drivers/ST7789/fonts.o: ../Drivers/ST7789/fonts.c \
- ../Drivers/ST7789/fonts.h
-../Drivers/ST7789/fonts.h:
